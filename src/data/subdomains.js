1a206817a9726c076acd6ae3d1739fec313715b4
karthi <karthifairhawn@gmail.com>
2025-04-09 18:55:07 +0530

    new changes

--- src/data/subdomains.js
+++ src/data/subdomains.js
@@ -1,32 +1,44 @@
 export const subdomains = [
   {
-    subdomain: 'About me',
-    description: 'Personal portfolio on my background, career and skills.',
-    url: 'https://karthi.infernus.in',
-    image: 'https://i.ibb.co/52sG4SL/freepik-candid-image-photography-natural-textures-highly-r-33120.jpg'
-  },
-  {
     subdomain: 'Blog',
-    description: 'I write me, my experience, philosophy, fictionatiol theories on life and beyond. ',
+    description: 'I write about my experience, philosophy, random thoughts on various topics. ',
     url: 'https://blog.infernus.in',
     image: 'https://i.ibb.co/JFr5k0X/freepik-candid-image-photography-natural-textures-highly-r-33121.jpg'
   },
   {
     subdomain: 'Immich',
-    description: ' fictionatiol theories on life and beyond. ',
+    description: 'It is my self hosted solution for photos, switched to immich due to high pricing in Gphotos. As it is self hosted it might be down most of time.Ill use whenever needed.',
     url: 'https://immich.infernus.in',
     image: 'https://repository-images.githubusercontent.com/455229168/ebba3238-9ef5-4891-ad58-a3b0223b12bd'
   },
   {
     subdomain: 'Astro Gallary',
-    description: 'I write me, my experience, philosophy, fictionatiol theories on life and beyond. ',
+    description: 'I love to watch stars, planets, galaxies and other celestial objects. I have a collection of photos taken by me with my 700mm/60 telescope. ',
     url: 'https://astro.infernus.in',
-    image: 'https://static.wanderon.in/wp-content/uploads/2024/06/stargazing-ladakh.jpg'
+    image: 'https://student-cms.prd.timeshighereducation.com/sites/default/files/styles/default/public/istock-664670026.jpg?itok=Q0zHmkJH'
   },
   {
     subdomain: 'Resume',
-    description: 'I write me, my experience, philosophy, fictionatiol theories on life and beyond. ',
+    description: 'My resume in a web format. This is clone of a template from nova resume maker, Made this bcoz I liked the resume template from nova but it was priced so. ',
     url: 'https://karthi-resume.infernus.in',
     image: 'https://img.freepik.com/free-vector/resume-writing-service-abstract-concept-illustration-copywriting-service-cv-online-professional-help-writing-resume-cover-letter-candidate-profile-career-summary_335657-143.jpg?semt=ais_hybrid'
+  },
+  {
+    subdomain: 'Sticky Note',
+    description: 'A Client only MD based sticky note app. I cant find a good sticky note app for my vibe so I made one for myself with bolt. Simple, clean and flexible,',
+    url: 'https://karthi-resume.infernus.in',
+    image: 'https://play-lh.googleusercontent.com/7_y7jAjtALGGHlVbClv0jq0gmA7iUovR7TOwdYIJ9zGbDxzsCD53nd-iW8v5j2AZ36k=w526-h296-rw'
+  },
+  {
+    subdomain: 'Java Class Signature Extractor',
+    description: 'A Client based Java class method signature extractor, I use collapse feature in vs code to collapse methods and see method definition. But found that it is not working(corrupting) on large junk class file, So made this simple tool to extract method signature',
+    url: 'https://extract-class-signature.infernus.in',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvRowB6hv9-RH1vvayIxz8yoEkzY5lyvSMDw&s'
+  },
+  {
+    subdomain: 'About me',
+    description: 'Portfolio on my background, career and skills. This is a old cloned template filled portfolio but I like it so keeping this on live.',
+    url: 'https://karthi.infernus.in',
+    image: 'https://i.ibb.co/52sG4SL/freepik-candid-image-photography-natural-textures-highly-r-33120.jpg'
   }
 ];
